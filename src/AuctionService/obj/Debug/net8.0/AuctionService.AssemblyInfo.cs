ed89d1b34396a1fc2f307463bbda54d30cb844c5
shabeer <mknshabeer@gmail.com>
2024-04-12 01:32:03 +0400

    New Contract Class for Auction Finished Bid

--- src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
+++ src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3873d7405002f60272fb18d3d279b4213325b0e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b59df6f5bef4a838caeff7a46d7754cfe4dce197")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
