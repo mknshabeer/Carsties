b59df6f5bef4a838caeff7a46d7754cfe4dce197
shabeer <mknshabeer@gmail.com>
2024-04-12 01:17:24 +0400

    Gateway Service

--- src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
+++ src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f59cf2c4edf2d1ac52dec2635e246eef5ba6a7dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3873d7405002f60272fb18d3d279b4213325b0e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
