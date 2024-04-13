41ab248434d989cccf0cf27239767a34a1b83c8b
shabeer <mknshabeer@gmail.com>
2024-04-13 20:00:52 +0400

    dockerizing auction service

--- src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
+++ src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b59df6f5bef4a838caeff7a46d7754cfe4dce197")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4704b26130234370b8f0ab74605147a8f918134d")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
